0bb48cff49f1dbe7349686abf126e3264529e540
TseringYuu <yuuzelin@outlook.com>
2022-04-07 22:46:23 +0800

    vue2周边技能：新生代vuex>>pinia开荒

---
+++ pinia/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    {{ counter }}
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia';
+import useCounterStore from '@/store';
+
+export default {
+  name: 'App',
+  computed: {
+    ...mapState({
+      counter: state => state.counter
+    })
+  },
+  methods: {
+    ...mapActions({
+      increment: 'increment'
+    })
+  }
+};
+</script>
+
